91bb27936b643cdf4b9ad40bf2088e308c41eed5
Abhigyan Kumar <abhigyan07k@gmail.com>
2026-01-04 22:32:59 +0530

    Update backend URL to Render deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-management-system-2-28wi.onrender.com/",
 });
 
 API.interceptors.request.use((config) => {
